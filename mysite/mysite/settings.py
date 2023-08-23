0c7fbfbc44bf9025620183f5995d352eb2f479c8
Matthew Hanson <mhanson.nflk@gmail.com>
2023-08-23 10:58:03 +0100

    turned off Django Debug by default

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-13719k%_klq0qlm6s0_35%m0rg%yg+056+%)w=(2q+=vqnffpu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
